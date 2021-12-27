bb8502b9790cc391c2b041ddbd2baa97a431228a
Nidal Hjab <NHjab@asaltech.com>
2021-12-27 15:21:16 +0200

    first commit

---
+++ apps/first-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import {Button} from '@nidalhjab/ui';
+function App() {
+  return (
+    <div className="App">
+      <p>hiiiiii</p>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
